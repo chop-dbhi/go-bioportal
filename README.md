e6acce06adefaa25d05ef9699f7159d4a7e8c242
Byron Ruth <b@devel.io>
2016-12-07 07:15:43 -0500

    Implement bioportal-to-neo4j tool
    Signed-off-by: Byron Ruth <b@devel.io>

--- README.md
+++ README.md
@@ -1,3 +1,58 @@
 # Go-Bioportal
 
 Go client for the [Bioontology Bioportal](http://bioportal.bioontology.org/) API.
+
+## Tools
+
+### Bioportal to Neo4j
+
+Takes a standard Bioportal-based CSV file of a vocabulary and generates CSV files and a script to import using the `neo4j-import` tool. Child-parent relationships are related using `subClassOf` and class-vocabulary relationship is modeled as `classOf`.
+
+
+#### Install
+
+Requires Go to install, however if you want pre-built binaries [please request it](https://github.com/chop-dbhi/go-bioportal/issues/)!
+
+```
+go get github.com/chop-dbhi/go-bioportal/cmd/bioportal-to-neo4j
+```
+
+#### Usage
+
+The command takes a vocab ID, label, and the file. It will output a set of files and print a script to standard out. The paths in the script will likely need to be adjusted, but it should serve as a template.
+
+```
+bioportal-to-neo4j <vocab-id> <vocab-label> <vocab-file>
+```
+
+#### Example
+
+- Download the ICD10-CM vocabulary
+- Run the tool
+- Show the output
+
+```
+$ curl -L http://data.bioontology.org/ontologies/ICD10CM/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb&download_format=csv > icd10cm.csv
+
+$ bioportal-to-neo4j \
+  icd10cm \
+  "International Classification of Diseases, Version 10 - Clinical Modification" \
+  icd10cm.csv > icd10cm_load.sh
+
+$ cat icd10cm_load.sh
+./neo4j/bin/neo4j-import \
+  --into ./neo4j/data/databases/graph.db \
+  --delimiter ',' \
+  --array-delimiter '|' \
+  --quote '"' \
+  --nodes:Vocabulary "icd10cm_nodes_vocabulary.csv" \
+  --nodes:Class "icd10cm_nodes_class.csv" \
+  --relationships "icd10cm_rels_subclassof.csv" \
+  --relationships "icd10cm_rels_classof.csv"
+
+$ ls
+icd10cm_nodes_class.csv      icd10cm_rels_classof.csv     icd10cm_load.sh
+icd10cm_nodes_vocabulary.csv icd10cm_rels_subclassof.csv
+
+$ bash icd10cm_load.sh  # assuming the paths are correct
+```
---
+++ cmd/bioportal-to-neo4j/main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+var vocabHeader = []string{
+	"id:ID(Vocabulary)",
+	"label",
+}
+
+var classHeader = []string{
+	"id:ID(Class)",
+	"label",
+	"code",
+	"synonyms:string[]",
+	"definitions:string[]",
+	"obsolete:boolean",
+	"cui:string[]",
+	"semanticTypes:string[]",
+}
+
+var subClassOfRelHeader = []string{
+	":START_ID(Class)",
+	":TYPE",
+	":END_ID(Class)",
+}
+
+var classOfRelHeader = []string{
+	":START_ID(Class)",
+	":TYPE",
+	":END_ID(Vocabulary)",
+}
+
+func newFile(name string, args ...interface{}) *os.File {
+	f, err := os.Create(fmt.Sprintf(name, args...))
+	if err != nil {
+		log.Fatal(f)
+	}
+	return f
+}
+
+func main() {
+	log.SetFlags(0)
+
+	args := os.Args[1:]
+	if len(args) < 2 {
+		log.Fatal("vocabulary id and label required")
+	}
+
+	vocabID := args[0]
+	vocabLabel := args[1]
+
+	cr := csv.NewReader(os.Stdin)
+	_, err := cr.Read()
+	if err != nil {
+		log.Fatalf("error reading row: %s", err)
+	}
+
+	f := newFile("%s_nodes_vocabulary.csv", vocabID)
+
+	cw := csv.NewWriter(f)
+	err = cw.WriteAll([][]string{
+		vocabHeader,
+		[]string{vocabID, vocabLabel},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	cw.Flush()
+	f.Close()
+
+	f = newFile("%s_nodes_class.csv", vocabID)
+	defer f.Close()
+	cw = csv.NewWriter(f)
+	defer cw.Flush()
+	if err := cw.Write(classHeader); err != nil {
+		log.Fatal(err)
+	}
+
+	vrf := newFile("%s_rels_classof.csv", vocabID)
+	defer vrf.Close()
+	vrcw := csv.NewWriter(vrf)
+	defer vrcw.Flush()
+	if err := vrcw.Write(classOfRelHeader); err != nil {
+		log.Fatal(err)
+	}
+
+	rf := newFile("%s_rels_subclassof.csv", vocabID)
+	defer rf.Close()
+	rcw := csv.NewWriter(rf)
+	defer rcw.Flush()
+	if err := rcw.Write(subClassOfRelHeader); err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		row, err := cr.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatalf("error reading row: %s", err)
+		}
+
+		id := row[0]
+		if id == "" {
+			log.Printf("skipping: %v", row)
+			continue
+		}
+
+		var parents []string
+		if row[7] != "" {
+			parents = strings.Split(row[7], "|")
+		}
+
+		toks := strings.Split(id, "/")
+		code := toks[len(toks)-1]
+
+		err = cw.Write([]string{
+			id,
+			row[1],
+			code,
+			row[2],
+			row[3],
+			row[4],
+			row[5],
+			row[6],
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = vrcw.Write([]string{
+			id,
+			"classOf",
+			vocabID,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, pid := range parents {
+			err = rcw.Write([]string{
+				id,
+				"subClassOf",
+				pid,
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	fmt.Printf(`./neo4j/bin/neo4j-import \
+	--into ./neo4j/data/databases/graph.db \
+	--delimiter ',' \
+	--array-delimiter '|' \
+	--quote '"' \
+	--nodes:Vocabulary "%s_nodes_vocabulary.csv" \
+	--nodes:Class "%s_nodes_class.csv" \
+	--relationships "%s_rels_subclassof.csv" \
+	--relationships "%s_rels_classof.csv"
+	`, vocabID, vocabID, vocabID, vocabID)
+}
